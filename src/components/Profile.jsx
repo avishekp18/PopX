a14b749c448002f73f3ebf6ebf4c72c767642b7c
avishekp18 <avishekpradhan24@gmail.com>
2025-09-18 21:25:57 +0530

    Initial commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
 // Main Dashboard Component
 import ProfileCard from "../components/Card"
+import avatarImage from "../assets/Avi.webp";
 export default function Dashboard() {
     const user = {
         name: "Avishek Pradhan",
         email: "avishekpradhan24@gmail.com",
         bio: "Full Stack Developer | MERN Enthusiast | Open to Opportunities",
-        avatar:
-            "https://media.licdn.com/dms/image/v2/D5603AQHNOXjr11LJSQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1719424449507?e=1761177600&v=beta&t=_LyP4hd6ozd6LMFkTUk5Kukg-jZfHJUSvbagD5SuKIc",
+        avatar: avatarImage,
         linkedin: "https://in.linkedin.com/in/avishek-pradhan0118052002",
     };
 
